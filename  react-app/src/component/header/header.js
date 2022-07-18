f754caa28378a897826664af81ad061b4793b144
Yashi2610 <yashi.jain@yash.com>
2022-07-18 10:37:59 +0530

    adding logout

---  react-app/src/component/header/header.js
+++  react-app/src/component/header/header.js
@@ -1,51 +1,104 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import { BiLogOut } from "react-icons/bi";
+import "./header.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import './header.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { FaUser } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import { GiWhiteBook } from "react-icons/gi";
+import { AiFillHome } from "react-icons/ai";
+import { GiHamburgerMenu } from "react-icons/gi";
+import "./dropdown.css";
+import { useUserAuth } from "../login/userAuthControl";
 
-import {FaUser} from "react-icons/fa";
-import {FaShoppingCart} from "react-icons/fa";
-import {GiWhiteBook} from "react-icons/gi";
-import {AiFillHome} from "react-icons/ai";
-import {GiHamburgerMenu} from "react-icons/gi"
+export function Header({ setShow, size }) {
+  const { logOut, user } = useUserAuth();
 
+  const navigate = useNavigate();
 
+  const handleLogout = async () => {
+    try {
+      await logOut();
 
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-export function Header({setShow, size }) {
- 
-  
+  const menuToggle = () => {
+    const toggleMenu = document.querySelector(".menu");
 
- 
+    toggleMenu.classList.toggle("active");
+  };
 
-   
-    
-  
   return (
+    <>
+      <nav>
+        <div className="logo">
+          <GiWhiteBook></GiWhiteBook>BookStore
+        </div>
 
-<>
+        <input type="checkbox" id="click"></input>
 
-<nav>
-              <div className="logo"><GiWhiteBook></GiWhiteBook>BookStore</div>
-              <input type="checkbox" id="click"></input>
-              <label htmlFor="click" className="menu-btn">
-              
-                <GiHamburgerMenu  className="menuicon" color="skyblue"></GiHamburgerMenu>
-              </label>
+        <label htmlFor="click" className="menu-btn">
+          <GiHamburgerMenu
+            className="menuicon"
+            color="skyblue"
+          ></GiHamburgerMenu>
+        </label>
+
+        <ul>
+          <li>
+            <a href="#">
+              <AiFillHome
+                color="skyblue"
+                size={30}
+                onClick={() => setShow(true)}
+              ></AiFillHome>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <FaShoppingCart
+                className="me-2"
+                color="skyblue"
+                size={30}
+                onClick={() => setShow(false)}
+              ></FaShoppingCart>
+
+              <span className="cartui me-5">{size}</span>
+            </a>
+          </li>
+          <div className="action">
+            <div className="profile">
+              <FaUser color="skyBlue" size={30} onClick={menuToggle}></FaUser>
+            </div>
+
+            <div className="menu">
+              <h3>{user && user.email}</h3>
+
+              {/* <h3>{user.displayName}</h3> */}
+
               <ul>
-                <li><a  href="#" ><AiFillHome color="skyblue" size={30} onClick={() => setShow(true)}></AiFillHome></a></li>&nbsp;&nbsp;
-                <li><a href="#"><FaUser color="skyblue" size={30}></FaUser></a></li>&nbsp;&nbsp;
-                <li><a href="#"><FaShoppingCart color="skyblue"size={30}  onClick={() => setShow(false)}></FaShoppingCart><span className='cartui'>{size}</span></a></li>
-                
+                <li>
+                  <a href="#">
+                    <span onClick={handleLogout}>Logout</span>
+
+                    <BiLogOut
+                      color="skyBlue"
+                      size={30}
+                      onClick={handleLogout}
+                    ></BiLogOut>
+                  </a>
+                </li>
               </ul>
-            </nav>
-
-            <div>
-        
-      </div> 
-
-
-</>
+            </div>
+          </div>
+        </ul>
+      </nav>
+    </>
   );
 }
-
---  react-app/src/component/login/userAuthControl.js
+++  react-app/src/component/login/userAuthControl.js
@@ -1,88 +1,42 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-import {
-
-  signInWithEmailAndPassword,
-
-  onAuthStateChanged,
-
-
-  
-
- 
-
-} from "firebase/auth";
+import { signInWithEmailAndPassword, onAuthStateChanged , signOut} from "firebase/auth";
 
 import { auth } from "./firebase";
 
-
-
 const userAuthContext = createContext();
 
-
-
 export function UserAuthContextProvider({ children }) {
-
   const [user, setUser] = useState({});
 
+  function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+  function logOut() {
 
-
-  function login(email,password) {
-  
-    return signInWithEmailAndPassword(auth, email,password);
+    return signOut(auth);
 
   }
 
-  
-
-  
-
-  
-
-
-
   useEffect(() => {
-
     const unsubscribe = onAuthStateChanged(auth, (currentuser) => {
-
       console.log("Auth", currentuser);
 
       setUser(currentuser);
-
     });
 
-
-
     return () => {
- 
       unsubscribe();
-
     };
-
   }, []);
 
-
-
   return (
-
-    <userAuthContext.Provider
-
-      value={{ user, login}}
-
-    >
-
+    <userAuthContext.Provider value={{ user, login ,logOut }}>
       {children}
-
     </userAuthContext.Provider>
-
   );
-
 }
 
-
-
 export function useUserAuth() {
-
   return useContext(userAuthContext);
-
 }
