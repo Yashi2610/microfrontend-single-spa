63bafc9901bb9d5a9e05ca88c5ec972d34d4b971
Yashi2610 <yashi.jain@yash.com>
2022-07-22 12:17:18 +0530

    adding another component

---  react-app/src/component/card/card.js
+++  react-app/src/component/card/card.js
@@ -1,40 +1,40 @@
-import React, {  useState } from 'react';
+import React, { useState } from "react";
 
+import Parcel from "single-spa-react/parcel";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+import { Header } from "../header/header";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "./home";
 
-import { Header } from '../header/header';
-
-import Home from './home';
-
-import Cart from '../cart/cart';
-import Footer from '../footer/footer';
-import { useUserAuth } from '../login/userAuthControl';
+import Cart from "../cart/cart";
+import Footer from "../footer/footer";
+import { useUserAuth } from "../login/userAuthControl";
 
 export function Card() {
   const [show, setShow] = useState(true);
 
   const [cart, setCart] = useState([]);
-  const {user} = useUserAuth();
-  console.log(user)
+  const { user } = useUserAuth();
+  console.log(user);
 
   const handleClick = (item) => {
-    console.log('item',item,cart)
-   const isProductExist = cart.find((product)=>product.id===item.id) 
-    if (isProductExist){
-      isProductExist.amount+=1
-      const newList  = cart.map((product)=>{if(product.id===item.id){
-        return isProductExist
-      }
-    return product} )
-      setCart(newList)
-    alert("one more item added in your cart");
-      return
-    };
+    console.log("item", item, cart);
+    const isProductExist = cart.find((product) => product.id === item.id);
+    if (isProductExist) {
+      isProductExist.amount += 1;
+      const newList = cart.map((product) => {
+        if (product.id === item.id) {
+          return isProductExist;
+        }
+        return product;
+      });
+      setCart(newList);
+      alert("one more item added in your cart");
+      return;
+    }
 
     setCart([...cart, item]);
-    
   };
 
   const handleChange = (item, d) => {
@@ -54,14 +54,16 @@ export function Card() {
 
       {show ? (
         <div>
-          
-        <Home handleClick={handleClick} />
-        <Footer/>
+          <Home handleClick={handleClick} />
+          <Parcel
+            config={() => System.import("@footer-angular/angular-project")}
+          />
+
+          {/* <Footer/> */}
         </div>
       ) : (
         <Cart cart={cart} setCart={setCart} handleChange={handleChange} />
       )}
-      
     </div>
   );
 }
